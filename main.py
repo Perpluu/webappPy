7b4744877190b7477eece216132636a57872a811
Perp <95175264+Perpluu@users.noreply.github.com>
2023-03-23 10:06:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
-from typing import List
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+products = [
+    {"name": "Product 1", "price": 10.99},
+    {"name": "Product 2", "price": 5.99},
+    {"name": "Product 3", "price": 15.99},
+    {"name": "Product 4", "price": 25.99}
+]
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
@@ -19,3 +25,7 @@ async def catalog(request: Request):
 @app.get("/products", response_class=HTMLResponse)
 async def products(request: Request):
     return templates.TemplateResponse("products.html", {"request": request, "title": "My Products", "current_year": 2023})
+
+@app.route("/cart")
+async def cart(request: Request):
+    return templates.TemplateResponse("cart.html", {"request": request, "products": products})
